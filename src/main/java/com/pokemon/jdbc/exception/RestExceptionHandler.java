87e81b16661ecad072f2585851386bde227e4b75
robert <robert.piotrowski92@gmail.com>
2018-12-03 18:45:23 +0100

    rest & service

--- src/main/java/com/pokemon/jdbc/exception/RestExceptionHandler.java
+++ src/main/java/com/pokemon/jdbc/exception/RestExceptionHandler.java
@@ -10,15 +10,13 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice(basePackages = "com.pokemon.rest")
-public class RestExceptionHandler {
-
-
-
+public class RestExceptionHandler  extends ResponseEntityExceptionHandler{
+    
 
     @ExceptionHandler(value = { RestPokemonException.class})
     public ResponseEntity<Object> handleRestPokemon(Exception e, WebRequest webRequest) {
         String errorText = "Couldn't add new pokemon";
-       // return handleExceptionInternal(e, errorText, new HttpHeaders(), HttpStatus.BAD_REQUEST, webRequest);
-return null;
+        return handleExceptionInternal(e, errorText, new HttpHeaders(), HttpStatus.BAD_REQUEST, webRequest);
+
     }
 }
